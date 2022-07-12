48bd43f7fcfc96e6ea418c53dbefa5803f0be440
Maja Knezevic <maja.knezevic1@gmail.com>
2022-07-12 12:24:29 +0200

    added assets folder and images for reels

---
+++ src/assets/images/index.js
@@ -0,0 +1,16 @@
+const IMAGES = {
+  apple: require("./apple.png"),
+  cherry: require("./cherry.png"),
+  seven: require("./seven.png"),
+  plum: require("./plum.png"),
+  strawberry: require("./strawberry.png"),
+  lemon: require("./lemon.png"),
+  banana: require("./banana.png"),
+  orange: require("./orange.png"),
+  tomato: require("./tomato.png"),
+  star: require("./star.png"),
+  watermelon: require("./watermelon.png"),
+  clover: require("./clover.png"),
+};
+
+export default IMAGES;
